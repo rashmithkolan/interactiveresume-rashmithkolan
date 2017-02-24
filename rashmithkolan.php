33a2518a5c1364f9080a4b9c4eff78a30ae03053
rashmithkolan <rashmith.kolan@gmail.com>
2017-02-24 11:37:33 -0500

    My personal website
    1st commit

---
+++ rashmithkolan.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Rashmith Kolan</title>
+		<link href = "styles.css" rel = "stylesheet" type = "text/css" />
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-nav">
+		    	<div class="navbar-header">
+			        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			          <span class="icon-bar"></span>
+			          <span class="icon-bar"></span>
+			          <span class="icon-bar"></span>                       
+			      	</button>
+		      	<a class="navbar-brand" href="#">Rashmith Kolan</a>
+    			</div>
+
+			<div>
+		      	<div class="collapse navbar-collapse" id="myNavbar">
+			        <ul class="nav navbar-nav navbar-right">
+			          <li><a class="page-scroll" href="#Profile">Profile</a></li>
+			          <li><a class="page-scroll" href="#Education">Education</a></li>
+			          <li><a class="page-scroll" href="#Experience">Experience</a></li>
+			          <li><a class="page-scroll" href="#Skillset">Skillset</a></li>
+<!--			          <li><a class="page-scroll" href="#Activities">Activities</a></li> !-->
+			          <li><a class="page-scroll" href="#Contactme">Contact Me</a></li>
+			         </ul>
+			          </li>
+			        </ul>
+		      	</div>
+    		</div>
+    	</div>
+    </nav>
+
+ <!--   <div id="top" class="jumbotron" img-src="/Users/RashmithKolan/Desktop/geek.png" data-position="center center">
+		</div>
+		!-->
+    	
+    	<div id="Profile" class="container-fluid">
+	   		<div class="row col-md-12 text-center">
+	   				<h1>Profile</h1>
+	   			<br>
+	   			<hr>
+	   		</div>	
+	   		
+	   		<div class="row">
+    			<div class = "col-md-4 text-justify">
+ 				<br>
+				    <h3 class="somethingabout">About Me</h3>
+						<p class="somethingabout">
+							I am a student doing my masters in information systems and have a combined experience of more than three years as a software engineer and System administrator. I have worked on PLM and ERP domains. I love to build things whether small or large, and have co-founded a photography startup "The Candid Diaries". <br>
+							Foodie. Traveller. Photographer. Coder. Gunner.</p>
+				</div>
+					
+					<div class="col-md-4 text-center"> 
+						<br>
+							<img src="/Users/RashmithKolan/Documents/Pics/Me2.jpg" alt = "Rashmith R Kolan" class="img-rounded" width = "246" height = "246">
+					</div>
+
+				<div class="col-md-4">
+					<br>	
+					<h3>Details</h3>
+						<b>Name:</b> 
+						<p>Rashmith R Kolan</p>
+						<b>Age:</b> 
+						<p>25 Years</p>
+						<b>Location:</b> 
+						<p>Mount Pleasant, Michigan, USA</p>				
+				</div>
+			</div>
+		</div>
+	
+
+		<div id="Education" class = "container-fluid">
+    		<div class="row">
+					<h1 class="text-center">Education</h1>
+					<br>
+					<hr>
+					<br>
+    			<div class="col-md-4">
+					<h4 class="Heading">Central Michigan University</h4>
+						<p class = "educationperiod Heading">January 2016 - December 2017</p>
+				</div>
+				<div class="col-md-8">		
+					<h4 class="something">Master of Science - Information Systems</h4>
+						<span class = "education-details text-justify"><p class="something">
+							Information systems is mainly about management and problem-solving skills needed to design, develop and use highly sought-after systems for analyzing business processes, manage projects and provide technical support. I am majoring in Business Intelligence and have gained experience on tools like SAS and Tableau.</p>
+						</span>
+				</div>
+			</div>
+				<br>
+			<div class="row">	
+				<div class = "col-md-4">	
+					<h4 class="Heading">Jawaharlal Nehru Tech U</h4>
+						<p class = "educationperiod Heading">August 2009 - May 2013</p>
+				</div>
+				<div class="col-md-8">		
+					<h4 class="something">Bachelor of Technology - Computer Science</h4>
+						<span class = "education-details text-justify"><p class="something">
+							Got introduced to the world of programming and the technologies that goes behind all the crazy stuff performed by these machines. Learnt about Operating systems, OOP Concepts, Programming languages, Web technologies and Databases.</p>
+						</span>
+				</div>
+			</div>
+		</div>
+
+    	<div id="Experience" class="container-fluid">
+	    	<div class="row">
+					<h1 class="text-center">Experience</h1>
+					<br>
+						<hr>
+					<br>
+				<div class="col-md-4">
+					<h4 class="Heading">Tech Mahindra</h4>
+					<p class="projectperiod Heading">January 2014 - January 2016</p>
+				</div>
+
+				<div class = "col-md-8 text-justify">
+						<h4 class="something1">Teamcenter PLM for GE Oil and Gas</h4>
+					<p class="something1">Worked in change management of BOM and document management module in TC. Good knowledge on workflow related issues. Conducting trend analysis on repeated issues and fixing the bugs. Extraction of EDM misaligned data and fixing it between TC and ERP system. Developing ITK utilities for various automation activites and providing support on user issues in production environment.
+					</p>
+					<h4 class="something1">JDEdwards ERP implementation for Renaissance Service</h4>
+					<p class="something1">Worked on an upgrade project of JDE for client organization writing the back end C business functions and generate UBE batch and interactive applications. Developed reports in Enterprise One 9.1 using Report Writer (RDA), developed various forms using Form Design Aid (FDA). Experience in Software Development Life Cycle (SDLC) includes requirements definition, design, testing, implementation, production support and enhancements of existing systems
+					</p>		
+				</div>
+			</div>
+				<br>
+			<div class="row">	
+				<div class="col-md-4">
+				<h4 class="Heading">Technology Operations</h4>
+					<p class="projectperiod Heading">January 2016 - (Yet to decide)</p>
+				</div>
+				<div class="col-md-8 text-justify">
+					<h4 class="something1">IT Expert</h4>
+					<p class="something1">
+						Responsible for gathering the requirements and suggest software according to the daily needs of end users. Provide day-to-day customer support operations both remotely and onsite. Manage automated software updates using Munki and Puppet tools for 800 workstations. Configure and deploy new workstations with OS X installed and add them to network through Manakin. Troubleshoot Macintosh computers while maintaining production level servers by implementing Munki, Manakin and Puppet. Printer management – upgrading firmware and security, administrating CUPS.
+					</p>	
+				</div>
+			</div>
+				<br>
+			<div class="row">	
+				<div class="col-md-4 Heading">
+					<h4 class="Heading">Central Michigan University</h4>
+						<p class="Heading">August 2016 - (Yet to decide)</p>
+				</div>
+				<div class="col-md-8 nobullets">
+					<h4 class="something1">Graduate Teaching Assistant</h4>
+							<p class="something1">
+								Assist students in learning concepts of Cybercrime and computer forensics
+								Prepare lab handouts, grading and conducting presentations
+							</p>
+				</div>
+			</div>
+		</div>
+
+    	<div id="Skillset" class="container-fluid text-center">
+				<h1>Skill Set</h1>
+				<br>
+					<hr>
+				<br>	
+					<div class="skillbars">
+						<h5>Web Technologies</h5>
+						<div class="progress skill-bar">
+			                <div class="progress-bar color1" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">HTML(5)<i class="val">100%</i></span>
+			                </div>
+			            </div>
+			                
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color2" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" >
+			                    <span class="skill">CSS(3)<i class="val">100%</i></span>
+			                </div>
+			            </div>
+			            
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color3" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">JavaScript<i class="val">70%</i></span>
+			                </div>
+			            </div>  
+
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color4" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" >
+			                    <span class="skill">BootStrap<i class="val">80%</i></span>
+			                </div>
+			            </div>
+
+			            <h5>Databases</h5>
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color6" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" >
+			                    <span class="skill">MySQL<i class="val">90%</i></span>
+			                </div>
+			            </div>
+			            
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color7" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">Oracle<i class="val">80%</i></span>
+			                </div>
+			            </div>  
+			            
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color8" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">SQL Server<i class="val">80%</i></span>
+			                </div>
+			            </div>
+
+			            <h5>Tools</h5>
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color5" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" >
+			                    <span class="skill">Teamcenter PLM<i class="val">65%</i></span>
+			                </div>
+			            </div>
+			            
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color9" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">JD Edwards ERP<i class="val">60%</i></span>
+			                </div>
+			            </div>
+
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color10" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">Tableau<i class="val">90%</i></span>
+			                </div>
+			            </div>
+
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color11" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" >
+			                    <span class="skill">Eclipse<i class="val">70%</i></span>
+			                </div>
+			            </div>
+			            
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color12" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">Photoshop<i class="val">70%</i></span>
+			                </div>
+			            </div>  
+			            
+			            <div class="progress skill-bar">
+			                <div class="progress-bar color13" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+			                    <span class="skill">Lightroom<i class="val">60%</i></span>
+			                </div>
+			            </div>
+			        </div>
+		</div>
+				<script>
+					$(document).ready(function() {
+		      			$('.skillbars .progress .progress-bar').css("width",
+		                	function() {
+		                    	return $(this).attr("aria-valuenow") + "%";
+		                	}
+		        		)
+		    		});
+				</script>
+
+
+    	<!--
+    	<div id="Activities" class="container-fluid">
+    		<div class="row nobullets">	
+				<h2 class="text-center">Activities</h2>
+				<ol class="something1">
+					<li>Works at The Candid Diaries</li>
+					<li>Design random logo's using photoshop</li>
+					<li>Verified food reviewer at Zomato - Ranked #2 in Mount Pleasant, MI</li>
+					<li>Love building things - Built an all terrain bot and won the first prize in intercollegiate robotic competition</li>
+					<li>Love to go places - A car, A stuffed backpack and a road is all I need</li>
+				</ol>	
+			</div>
+		</div>
+		!-->
+
+		<div id="Contactme" class="container-fluid">
+			<div class="row col-md-12 text-center">
+				<h1>Contact</h1>
+				<br>
+					<hr>
+				<br>
+				<p>
+					<a href="https://www.facebook.com/rashmithreddy.kolan" target="_blank" title="fb"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            	<a href="https://twitter.com/rashmithreddy" target="_blank" title="twitter"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            	<a href="https://plus.google.com/+RashmithReddy" target="_blank"><i class="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+	            	<a href="mailto:rashmith.kolan@gmail.com" target="_blank"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>	            	
+					<a href="https://www.linkedin.com/in/rashmithrkolan" target="blank" title="Linkedin"><i id="social-in" class="fa fa-linkedin-square fa-3x social"></i></a>
+				</p>
+			</div>
+		</div>
+
+	</body>
+</html>
